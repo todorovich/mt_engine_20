4d0d0e37635057518aab41135ac9d700a06a55e2
Micho Todorovich <micho.todorovich@gmail.com>
2022-10-18 15:54:31 -0500

    differentiated HELD and PRESSED

--- Engine/source/Engine/windows/messages/WM_KeyDown.cpp
+++ Engine/source/Engine/windows/messages/WM_KeyDown.cpp
@@ -10,11 +10,13 @@ import InputManager;
 
 LRESULT mt::windows::WM_KeyDown::execute(const HWND& hwnd, const UINT& msg, const WPARAM& wParam, const LPARAM& lParam)
 {
-	//OutputDebugStringW(L"WM_KEY_UP_Handler\n");
+	auto key_held_mask = 0x40000000;
+
+	bool repeated = lParam & key_held_mask;
 
 	_input_manager->KeyboardEvent(
 		static_cast<mt::input::KeyboardKeys>(wParam),
-		mt::input::KeyState::PRESSED
+		repeated ? mt::input::KeyState::HELD : mt::input::KeyState::PRESSED
 	);
 
 	// An application should return zero if it processes this message.
